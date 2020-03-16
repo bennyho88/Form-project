2463b5ba25bf9655b768bf1908597645fb46d671
Benny Ho <benny.ho2530@gmail.com>
2020-03-16 14:17:57 +0100

    added logics for validatefield method

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@
 
         const display = new Display();
         display.checkFields();
+        display.hideSubmit();
     })
 
     // display 
@@ -20,7 +21,7 @@
     // check fields
 
     Display.prototype.checkFields = function() {
-        console.log(this);
+      //  console.log(this);
         this.name.addEventListener('blur', this.validateField);
         this.course.addEventListener('blur', this.validateField);
         this.author.addEventListener('blur', this.validateField);
@@ -29,7 +30,34 @@
     // validate each field
 
     Display.prototype.validateField = function() {
-        console.log(this);
+       // console.log(this);
+       if(this.value === '') {
+        this.classList.remove('complete');
+        this.classList.add('fail');
+       } else {
+        this.classList.remove('fail');
+        this.classList.add('complete');
+
+       }
+
+       const complete = document.querySelectorAll('.complete');
+       console.log(complete.length);
+       
+       if(complete.length === 3) {
+           document.querySelector('.submitBtn').disabled = false;
+           
+       } else {
+           document.querySelector('.submitBtn').disabled = true;
+       }
+
+    };
+
+    // disable submit button
+    Display.prototype.hideSubmit = function() {
+
+        const button = document.querySelector('.submitBtn');
+
+        button.disabled = true;
     }
 
 })();
